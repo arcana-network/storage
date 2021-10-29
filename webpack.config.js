9baff18f8779f350cb012c7c5b341d69f87c4bf7
SauravKanchan <sauravnk30@gmail.com>
2021-10-29 14:54:32 +0530

    remove dkg_client

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,39 @@
 const path = require('path');
-const NodePolyfillPlugin = require("node-polyfill-webpack-plugin")
-
+const NodePolyfillPlugin = require('node-polyfill-webpack-plugin');
 
 const serverConfig = () => {
-    return {
-        entry: {
-            "arcana": path.resolve(__dirname, "src", "index.ts")
+  return {
+    entry: {
+      arcana: path.resolve(__dirname, 'src', 'index.ts'),
+    },
+    mode: 'production',
+    module: {
+      rules: [
+        {
+          test: /\.(ts|js)x?$/,
+          use: {
+            loader: 'ts-loader',
+            options: {
+              configFile: 'tsconfig.json',
+            },
+          },
+          exclude: /node_modules/,
         },
-        mode: 'production',
-        module: {
-            rules: [{
-                test: /\.(ts|js)x?$/,
-                use: {
-                    loader: 'ts-loader',
-                    options: {
-                        configFile: 'tsconfig.json'
-                    }
-                },
-                exclude: /node_modules/,
-            }]
-        },
-        resolve: {
-            extensions: ['.tsx', '.ts', '.js', '.json'],
-            fallback: {
-                crypto: require.resolve("crypto-browserify"),
-                stream: require.resolve("stream-browserify")
-            }
-        },
-        output: {
-            filename: x => x.chunk.name + '.js',
-            library: '[name]',
-            path: path.resolve(__dirname, 'dist'),
-        }
-    }
+      ],
+    },
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js', '.json'],
+      fallback: {
+        crypto: require.resolve('crypto-browserify'),
+        stream: require.resolve('stream-browserify'),
+      },
+    },
+    output: {
+      filename: (x) => x.chunk.name + '.js',
+      library: '[name]',
+      path: path.resolve(__dirname, 'dist'),
+    },
+  };
 };
 
 module.exports = serverConfig;
