3b04f3746f9214f87c18635f023f49215ee725e2
mmjee <root@git.maharshi.ninja>
2022-05-23 21:27:03 +0530

    Possible sentry integration
    Wrap further instances

---
+++ src/sentry.ts
@@ -0,0 +1,29 @@
+import { captureException } from "@sentry/browser";
+
+const reportedErrors = new Set()
+
+export function wrapMethod (ctx, fn) {
+  // tslint:disable-next-line:only-arrow-functions
+  return async (...args) => {
+    try {
+      return await fn.apply(ctx, args)
+    } catch (e) {
+      if (!reportedErrors.has(e)) {
+        captureException(e)
+        reportedErrors.add(e)
+      }
+      throw e
+    }
+  }
+}
+
+export function wrapInstance (ins) {
+  // We actually need to iterate over inherited properties
+  // tslint:disable-next-line:forin
+  for (const insKey in ins) {
+    const v = ins[insKey]
+    if (typeof v === 'function') {
+      ins[insKey] = wrapMethod(ins, v)
+    }
+  }
+}
