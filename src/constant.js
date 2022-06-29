7f7567d4193f3a8ce6f462374791f60906c088e6
r0hith <44583503+naraharirohith@users.noreply.github.com>
2022-06-29 14:43:04 +0530

    Removed gateway while initializing storage instance (#75)
    * Removed gateway while initializing storage instance
    
    * given gateway or gateway from chainId

--- src/constant.js
+++ src/constant.js
@@ -1,3 +1,7 @@
 export const readHash = "0x7265616400000000000000000000000000000000000000000000000000000000";
 export const chainId = 40405;
-export const gateway = "https://gateway001-testnet.arcana.network/api/v1/"
+
+export var chainIdToGateway = new Map([
+    [40405, "https://gateway001-testnet.arcana.network/api/v1/"],
+    [40404, "https://gateway-testnet.arcana.network/api/v1/"]
+]);
