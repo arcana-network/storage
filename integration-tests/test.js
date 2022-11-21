c8acb79397fbc22c7f1c7acb826ec670d8871aed
Saurav  Kanchan <sauravnk30@gmail.com>
2022-11-21 10:17:09 +0530

    update abi (#169)

--- integration-tests/test.js
+++ integration-tests/test.js
@@ -1,14 +1,8 @@
-// const { expect } = require("chai");
 const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-// const gateway = 'https://gateway-testnet.arcana.network/';
-const gateway = 'http://localhost:9010/';
-// const gateway = 'https://gateway01-testnet.arcana.network/';
-// const gateway = 'https://gateway-dev.arcana.network/';
-const chainId = 40404;
-const appId = 613;
-// const appId = 28;
 const delay = ms => new Promise(res => setTimeout(res, ms));
-
+const appAddress = "Fd3d7b91fbB83c418697C66F134E06193590Ff90"
+const chainId = 40404;
+const gateway = "http://localhost:9010/"
 
 const debug = false;
 const generateString = (length) => {
@@ -57,25 +51,17 @@ const makeEmail = () => {
 
 describe('Upload File', () => {
   let file,
-    did,
-    arcanaInstance,
-    access,
-    receiverWallet,
-    sharedInstance,
-    file_count = 0;
+    did1,
+    arcanaInstance;
 
 
   before(async () => {
     file = MockFile('qwertyuiopasdfghjklzxcvbnmqwertyuiop.txt', 10, 'text/plain');
     file = new File([file], file.name, { type: file.type });
     arcanaInstance = await arcana.storage.StorageProvider.init({
-      appId,
-      // appAddress: "445007f942f9Ba718953094Bbe3205B9484cAfd2",
-      provider: window.ethereum,
-      email: makeEmail(),
+      appAddress,
       chainId,
-      gateway,
-      debug,
+      gateway
     });
     let allFiles = await arcanaInstance.files.all()
     console.log({allFiles})
@@ -83,44 +69,33 @@ describe('Upload File', () => {
 
 
 
-  it.only('Upload', async () => {
-    let my_file_old = await arcanaInstance.files.list(arcana.storage.AccessTypeEnum.MY_FILES);
-    did = await arcanaInstance.upload(file);
-    console.log('did', did);
-    let my_file_new = await arcanaInstance.files.list(arcana.storage.AccessTypeEnum.MY_FILES);
-    chai.expect(my_file_new.length - my_file_old.length).equal(1); 
-    console.log({my_file_new})
+  it('Upload', async () => {
+    did1 = await arcanaInstance.upload(file);
+    console.log(did1)
   });
 
+  it.skip('Download', async () => {
+    await arcanaInstance.download(did1)  
+  })
 
   it('Share', async () => {
-    // let did = "0257b566bc3fea825635298d1c8565d11c94bf6e5a697643c11b43c129b6c13b"
     let address = "0xbd92a7c9BF0aE4CaaE3978f9177A696fe7eA179F"
-    // let address2 = "0x64b69590954570d63bb60b9bba4ab3814f1a3a22"
-    let users = await arcanaInstance.files.getSharedUsers(did)
-    console.log({users})
-    await arcanaInstance.files.share(did, address)
-    // await arcanaInstance.files.revoke(did, address)
-    users = await arcanaInstance.files.getSharedUsers(did)
-    console.log({users})
+    await arcanaInstance.files.share(did1, address)
     alert("change account")
-    // await arcanaInstance.files.share(did, address2)
-    // await arcanaInstance.files.revoke(did, address)
+    let store = await arcana.storage.StorageProvider.init({
+      appAddress,
+      chainId,
+      gateway
+    });
+    await store.download(did1)  
   });
 
-  it.only('Download', async () => {
-    // let store = await arcana.storage.StorageProvider.init({
-    //   appId,
-    //   provider: window.ethereum,
-    //   email: makeEmail(),
-    //   chainId,
-    //   gateway,
-    //   debug,
-    // });
-    // let files = await store.files.list(arcana.storage.AccessTypeEnum.SHARED_FILES)
-    // console.log({files})
-    // await store.download(did)  
-    await arcanaInstance.download(did)  
+
+
+  it.skip('DownloadNFT', async () => {
+    // did1 = "02220d65fe78758cb650441217004b9fa79249f171918055af29b85fba947e37";
+    await arcanaInstance.linkNft(did1, 1, "0x35B33203DcFd0933a773B28C9Fce13012B5a1434", 80001)
+    await arcanaInstance.download(did1)
   })
 
   it.skip("Delete file", async () => {
